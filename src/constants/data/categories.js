350a65d4834ebbbfd103009baf8408c38c4b55f0
agustinSC2034 <aguustinn18@gmail.com>
2023-05-20 01:15:32 -0300

    20-5-23

--- src/constants/data/categories.js
+++ src/constants/data/categories.js
@@ -1,27 +1,27 @@
 export const CATEGORIES = [
   {
     id: 1,
-    name: "Harinas Puras",
+    name: "Nike",
     color: "#F4E9CD",
   },
   {
     id: 2,
-    name: "Harinas Integrales",
+    name: "Adidas",
     color: "#A9C6C3",
   },
   {
     id: 3,
-    name: "Harinas de Trigo",
+    name: "Puma",
     color: "#6BAAB3",
   },
   {
     id: 4,
-    name: "Harinas de Maíz",
+    name: "Fila",
     color: "#68C0F3",
   },
   {
     id: 5,
-    name: "Harinas de Arroz",
+    name: "Topper",
     color: "#E9D39B",
   },
 ];
